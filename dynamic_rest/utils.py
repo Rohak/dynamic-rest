abfa1ac0df936af41d47e32a4b83eabc36a1cc3b
Rohak Muravsky <rohak@madalbal.cz>
2020-07-14 17:28:41 +0200

    remove django.utils.six import

--- dynamic_rest/utils.py
+++ dynamic_rest/utils.py
@@ -1,5 +1,3 @@
-from django.utils.six import string_types
-
 FALSEY_STRINGS = (
     '0',
     'false',
@@ -8,7 +6,7 @@ FALSEY_STRINGS = (
 
 
 def is_truthy(x):
-    if isinstance(x, string_types):
+    if isinstance(x, str):
         return x.lower() not in FALSEY_STRINGS
     return bool(x)
 
